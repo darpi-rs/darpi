3d3ff35f926d35407b5fec40c603eb05083b1b21
Petar Dambovaliev <petar.atanasov.1987@gmail.com>
2021-05-05 13:20:27 +0200

    fix ws test

--- tests/web_socket.rs
+++ tests/web_socket.rs
@@ -1,44 +1,17 @@
 use darpi::futures::{SinkExt, StreamExt};
+use darpi::upgrade::Upgraded;
 use darpi::{app, handler, job::FutureJob, response::UpgradeWS, Body, Request};
 use futures::Future;
-use http::header::{CONNECTION, SEC_WEBSOCKET_KEY, UPGRADE};
-use http::StatusCode;
 use rand::Rng;
 use std::sync::Once;
+use tokio::net::TcpStream;
 use tokio::sync::oneshot::{Receiver, Sender};
-use tokio_tungstenite::{
-    connect_async, tungstenite::protocol::Message, tungstenite::protocol::Role, WebSocketStream,
-};
-
-mod common;
-
-use common::convert_key;
-use reqwest::Response;
 use tokio_tungstenite::tungstenite::error::Error;
+use tokio_tungstenite::{client_async, tungstenite::protocol::Message, WebSocketStream};
 
 pub const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~";
-
-async fn make_req() -> Response {
-    let client = reqwest::Client::new();
-
-    let req = client
-        .get("http://127.0.0.1:3000")
-        .header(CONNECTION, "upgrade")
-        .header(UPGRADE, "websocket")
-        .header(SEC_WEBSOCKET_KEY, convert_key("password".as_bytes()))
-        .build()
-        .unwrap();
-
-    let resp = client.execute(req).await.unwrap();
-    assert_eq!(StatusCode::SWITCHING_PROTOCOLS, resp.status());
-    assert_eq!(Some(0), resp.content_length());
-    assert_eq!("websocket", resp.headers().get(UPGRADE).unwrap());
-    assert_eq!("upgrade", resp.headers().get(CONNECTION).unwrap());
-
-    resp
-}
 
 #[tokio::test]
 async fn websocket() {
@@ -46,51 +19,32 @@ async fn websocket() {
     tokio::spawn(app);
     startup.await.unwrap();
 
-    make_req().await;
+    let addr = "127.0.0.1:3000";
+    let url = format!("ws://{}/websocket", addr);
 
-    let (mut ws_stream, _) = connect_async("ws://127.0.0.1:3000")
-        .await
-        .expect("Failed to connect");
+    let stream = TcpStream::connect(addr).await.unwrap();
 
-    let mut msgs = vec![];
+    let (mut ws_stream, _) = client_async(&url, stream).await.unwrap();
+
     for _ in 0..10 {
-        let rand_str_clone = make_rand_str();
-        msgs.push(rand_str_clone);
-    }
+        let rand_str = make_rand_str();
+        let rand_str_clone = rand_str.clone();
+        ws_stream.send(rand_str.into()).await.unwrap();
+        let msg = ws_stream.next().await.unwrap();
 
-    let (rx, mut tx) = tokio::sync::mpsc::channel(10);
-
-    let msg_clone = msgs.clone();
-    let join = tokio::spawn(async move {
-        let mut i = 0usize;
-        while let Some(msg) = tx.recv().await {
-            ws_stream.send(msg).await.unwrap();
-            let msg = ws_stream.next().await.unwrap();
-
-            let msg = match msg {
-                Ok(m) => m,
-                Err(e) => {
-                    panic!("client error trying to receive:  `{:#?}`", e);
-                }
-            };
-
-            if msg.is_text() || msg.is_binary() {
-                assert_eq!(msg.to_string(), msg_clone[i]);
+        let msg = match msg {
+            Ok(m) => m,
+            Err(e) => {
+                panic!("client error trying to receive:  `{:#?}`", e);
             }
-            i += 1;
+        };
+
+        if msg.is_text() || msg.is_binary() {
+            assert_eq!(msg.to_string(), rand_str_clone);
         }
-        assert_eq!(i, msg_clone.len());
-
-        ws_stream.close(None).await.unwrap();
-        SinkExt::close(&mut ws_stream).await.unwrap();
-        println!("closing client websocket");
-    });
-
-    for msg in msgs {
-        rx.send(Message::from(msg)).await.unwrap();
     }
-    drop(rx);
-    join.await.unwrap();
+
+    ws_stream.close(None).await.unwrap();
     shutdown.send(()).unwrap();
 }
 
@@ -106,6 +60,39 @@ fn make_rand_str() -> String {
     rand_str
 }
 
+async fn handle_connection(stream: Upgraded) {
+    let mut ws_stream = WebSocketStream::from_raw_socket(
+        stream,
+        tokio_tungstenite::tungstenite::protocol::Role::Server,
+        None,
+    )
+    .await;
+
+    while let Some(item) = ws_stream.next().await {
+        match item {
+            Ok(msg) => match msg {
+                Message::Text(text) => {
+                    ws_stream.send(Message::Text(text)).await.unwrap();
+                }
+                Message::Close(_) => {
+                    if let Err(e) = ws_stream.close(None).await {
+                        match e {
+                            Error::ConnectionClosed => (),
+                            _ => {
+                                println!("Error while closing: {:#?}", e);
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+                _ => (),
+            },
+            Err(e) => println!("server error {:?}", e),
+        }
+    }
+}
+
 #[handler]
 async fn web_socket_handler(#[request] r: Request<Body>) -> Result<UpgradeWS, String> {
     let resp = UpgradeWS::from_header(r.headers())
@@ -113,31 +100,7 @@ async fn web_socket_handler(#[request] r: Request<Body>) -> Result<UpgradeWS, St
 
     FutureJob::from(async move {
         let upgraded = darpi::upgrade::on(r).await.unwrap();
-        let mut ws_stream = WebSocketStream::from_raw_socket(upgraded, Role::Server, None).await;
-
-        while let Some(msg) = ws_stream.next().await {
-            let msg = match msg {
-                Ok(m) => m,
-                Err(e) => {
-                    match e {
-                        Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
-                        err => panic!("Testcase failed: {:#?}", err),
-                    }
-                    return;
-                }
-            };
-
-            if msg.is_text() || msg.is_binary() {
-                if let Err(e) = ws_stream.send(msg).await {
-                    panic!("error trying to send:  `{:#?}`", e);
-                }
-            } else if msg.is_close() {
-                ws_stream.close(None).await.unwrap();
-                SinkExt::close(&mut ws_stream).await.unwrap();
-                println!("closing server websocket");
-                return;
-            }
-        }
+        handle_connection(upgraded).await;
     })
     .spawn()
     .map_err(|e| format!("{}", e))?;
@@ -149,13 +112,14 @@ static ONCE: Once = Once::new();
 
 pub fn make_server() -> (Sender<()>, Receiver<()>, impl Future<Output = ()>) {
     ONCE.call_once(|| {
+        //std::env::set_var("RUST_LOG", "debug");
         env_logger::builder().is_test(true).init();
     });
 
     let mut app = app!({
         address: "127.0.0.1:3000",
         handlers: [{
-            route: "/",
+            route: "/websocket",
             method: GET,
             handler: web_socket_handler
         }]
